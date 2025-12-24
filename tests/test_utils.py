99305f4e99d5bdabb1ea8f11660b16b28405004a
jirehhuang <jirehhuang@ucla.edu>
2025-12-23 16:35:41 -0800

    test: add test for read file as string

---
+++ tests/test_utils.py
@@ -0,0 +1,61 @@
+"""Test utilities module."""
+
+from pathlib import Path
+
+import pytest
+
+from bibletools._utils import _read_file_as_string
+
+KJV_XML_URL = (
+    "https://raw.githubusercontent.com/rwev/bible/refs/heads/master/bible/"
+    "translations/KJV.xml"
+)
+KJBIBLE_XML_URL = (
+    "https://raw.githubusercontent.com/Beblia/Holy-Bible-XML-Format/"
+    "refs/heads/master/EnglishKJBible.xml"
+)
+
+
+@pytest.mark.parametrize(
+    "file_location",
+    [
+        KJV_XML_URL,
+        KJBIBLE_XML_URL,
+        "KJV.xml",
+        (
+            str(
+                Path(__file__).resolve().parents[1]
+                / "bibletools"
+                / "data"
+                / "translations"
+                / "EnglishKJBible.xml"
+            )
+        ),
+    ],
+)
+def test_read_file_as_string(file_location):
+    """Test that the _read_file_as_string function correctly reads a specified
+    file location that could be a URL, file name, or file path."""
+    xml = _read_file_as_string(file_location=file_location)
+    assert isinstance(xml, str)
+    assert xml.startswith("<?xml")
+    for verse_text in [
+        # Psalm 16:11
+        (
+            "Thou wilt shew me the path of life: in thy presence is fulness "
+            "of joy; at thy right hand there are pleasures for evermore."
+        ),
+        # Ephesians 2:8
+        (
+            "For by grace are ye saved through faith; and that not of "
+            "yourselves: it is the gift of God:"
+        ),
+        # Philippians 1:21
+        "For to me to live is Christ, and to die is gain.",
+        # Proverbs 18:17
+        (
+            "He that is first in his own cause seemeth just; but his "
+            "neighbour cometh and searcheth him."
+        ),
+    ]:
+        assert verse_text in xml
