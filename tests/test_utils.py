5ad7fce956a3c3cf70ce6410df8c670c811447c6
jirehhuang <jirehhuang@ucla.edu>
2025-12-24 09:20:34 -0800

    refactor(test): update to use str chapter and verse numbers instead of integer

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -71,14 +71,18 @@ def test_parse_xml_to_dict(file_location):
     xml = _read_file_as_string(file_location=file_location)
     bible_dict = _parse_xml_to_dict(xml=xml)
     assert isinstance(bible_dict, dict)
-    assert bible_dict["Psalms"][16][11]["text"] == VERSE_TEXTS["Psalm 16:11"]
     assert (
-        bible_dict["Ephesians"][2][8]["text"] == VERSE_TEXTS["Ephesians 2:8"]
+        bible_dict["Psalms"]["16"]["11"]["text"] == VERSE_TEXTS["Psalm 16:11"]
     )
     assert (
-        bible_dict["Philippians"][1][21]["text"]
+        bible_dict["Ephesians"]["2"]["8"]["text"]
+        == VERSE_TEXTS["Ephesians 2:8"]
+    )
+    assert (
+        bible_dict["Philippians"]["1"]["21"]["text"]
         == VERSE_TEXTS["Philippians 1:21"]
     )
     assert (
-        bible_dict["Proverbs"][18][17]["text"] == VERSE_TEXTS["Proverbs 18:17"]
+        bible_dict["Proverbs"]["18"]["17"]["text"]
+        == VERSE_TEXTS["Proverbs 18:17"]
     )
