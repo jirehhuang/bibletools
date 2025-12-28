3eefc61024477b08564fb4bfe0c532567628fa82
jirehhuang <jirehhuang@ucla.edu>
2025-12-28 08:48:57 -0800

    refactor: expose public methods

--- bibletools/__init__.py
+++ bibletools/__init__.py
@@ -1,3 +1,12 @@
 """Toolkit for working with the Bible."""
 
+from ._get_verses import (
+    convert_reference_to_verse_text,
+    get_all_verse_ids,
+    get_highest_weighted_verse,
+    get_random_verse_id,
+    get_random_verse_ids,
+    load_verse_counts,
+)
 from ._parse_references import parse_references
+from ._verse_text_map import parse_xml_to_verse_text_map
